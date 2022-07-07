59a2a46eded599d0f61901662226d3fc7f8fd67c
Jean-CODE-Van-Damme <oreverseau@live.fr>
2022-07-07 22:55:51 +0200

    resp ok

--- src/App.js
+++ src/App.js
@@ -4,20 +4,83 @@ import Form from "./components/Form";
 import StepTwo from "./components/StepTwo";
 import Footer from "./Footer";
 
-let str = "manue";
-let newStr = "";
-newStr = str.substring(0, 2);
-console.log(newStr);
-
 function App() {
-  // const [name, setName] = useState("");
-  // const [email, setEmail] = useState("");
-  // const [password, setPassword] = useState("");
-  // const [passwordConfirm, setPasswordConfirm] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [error, setError] = useState(false);
+  const [formOk, setFormOk] = useState(false);
+
   return (
-    <div>
-      <Form />
-      <StepTwo />
+    <div className="big-div">
+      {formOk ? (
+        <StepTwo
+          setFormOk={setFormOk}
+          title="Results"
+          name={name}
+          setName={setName}
+          email={email}
+          setEmail={setEmail}
+          password={password}
+          setPassword={setPassword}
+          passwordConfirm={passwordConfirm}
+          setPasswordConfirm={setPasswordConfirm}
+          errorMessage={errorMessage}
+          setErrorMessage={setErrorMessage}
+          error={error}
+          setError={setError}
+        />
+      ) : (
+        <Form
+          setFormOk={setFormOk}
+          title="Create Account"
+          name={name}
+          setName={setName}
+          email={email}
+          setEmail={setEmail}
+          password={password}
+          setPassword={setPassword}
+          passwordConfirm={passwordConfirm}
+          setPasswordConfirm={setPasswordConfirm}
+          errorMessage={errorMessage}
+          setErrorMessage={setErrorMessage}
+          error={error}
+          setError={setError}
+        />
+      )}
+      {/* <Form
+        setFormOk={setFormOk}
+        title="Create Account"
+        name={name}
+        setName={setName}
+        email={email}
+        setEmail={setEmail}
+        password={password}
+        setPassword={setPassword}
+        passwordConfirm={passwordConfirm}
+        setPasswordConfirm={setPasswordConfirm}
+        errorMessage={errorMessage}
+        setErrorMessage={setErrorMessage}
+        error={error}
+        setError={setError}
+      /> */}
+      {/* <StepTwo
+        title="Results"
+        name={name}
+        setName={setName}
+        email={email}
+        setEmail={setEmail}
+        password={password}
+        setPassword={setPassword}
+        passwordConfirm={passwordConfirm}
+        setPasswordConfirm={setPasswordConfirm}
+        errorMessage={errorMessage}
+        setErrorMessage={setErrorMessage}
+        error={error}
+        setError={setError}
+      /> */}
       <Footer />
     </div>
   );
--- src/components/StepTwo.js
+++ src/components/StepTwo.js
@@ -1,5 +1,43 @@
-const StepTwo = (props) => {
-  return;
+import Footer from "../Footer";
+
+const StepTwo = ({
+  title,
+  name,
+  setName,
+  email,
+  setEmail,
+  password,
+  setPassword,
+  passwordConfirm,
+  setPasswordConfirm,
+  errorMessage,
+  setErrorMessage,
+  error,
+  setError,
+  setFormOk,
+}) => {
+  return (
+    <>
+      <h1>{title}</h1>
+
+      <div className="steptwo">
+        <div className="results">
+          <p> Name : {name}</p>
+          <p> Email : {email}</p>
+          <p> Password : {password}</p>
+        </div>
+        <div className="button2">
+          <button
+            onClick={() => {
+              setFormOk(false);
+            }}
+          >
+            Edit Your Information
+          </button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default StepTwo;
